24de007bb5bdc1b640143716941a6b5c808308a5
N-Cantu <129448328+N-Cantu@users.noreply.github.com>
2023-04-11 15:56:26 -0500

    Final Project Code beta 3
    Added the beta loop code for the servos
    This would be the basis for any future changes, we still need to think about how we are going to go about it.

--- Final_Project_beta.ino
+++ Final_Project_beta.ino
@@ -1,11 +1,12 @@
 //Add 2 button features (completed)
-//Add a sensor feature (if we can)
-//Add a light feature, where the button on the sun can only activate once the other servo interacts with the sensor
+//Placeholder comment
+//Add a light feature, where the button on the sun can only activate once the other servo interacts with something (no clue on the work around as of now)
 //Try to brainstorm and improve on current code
 
 //Servo set-up
 #include <Servo.h>
-Servo towerservo;  //create
+Servo towerservo;  //creates a servo variable for the tower
+Servo sunservo;    //creates a servo variable for the sun
 
 //Tower button set-up
 const int buttonTPin = 4;  //Placeholder pin number
@@ -20,11 +21,12 @@ int prevButtonSState = 0;
 bool S = false;
 
 void setup() {
-  
+  sunservo.attach(9);
+  towerservo.attach(10);
   pinMode(buttonTPin, INPUT);
   pinMode(buttonSPin, INPUT);
   Serial.begin(9600);
-  Serial.println("Start Tower Button")
+  Serial.println("Start Tower Button");
 }
 
 void loop() {
@@ -34,9 +36,31 @@ void loop() {
   if (buttonTState != prevButtonTState) {
     if (buttonTState == HIGH) {
       T = true;
+      towerservo.write(90);
       Serial.println("Start Sun button");
+    } else {
+      towerservo.write(0);
     }
   }
-}
 
-//
+  //if tower button is true, read the sun button
+  if (T == true) {
+    buttonSState = digitalRead(buttonSPin);
+    if (buttonSState != prevButtonSState) {
+      if (buttonSState == HIGH) {
+        S = true;
+        sunservo.write(90);
+        Serial.println("Start Sun button");
+      } else {
+        sunservo.write(0);
+      }
+    }
+  }
+
+  if (S == true) {
+    Serial.println("The End");
+  }
+
+  prevButtonTState = buttonTState;
+  prevButtonSState = buttonSState;
+}
