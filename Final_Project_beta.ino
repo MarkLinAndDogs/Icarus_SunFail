7cb14f179fdf3342786e31ce4cdda1b9ab4c9a92
N-Cantu <129448328+N-Cantu@users.noreply.github.com>
2023-04-25 15:06:01 -0500

    Add files via upload

--- Final_Project_beta.ino
+++ Final_Project_beta.ino
@@ -8,6 +8,9 @@
 Servo towerservo;  //creates a servo variable for the tower
 Servo sunservo;    //creates a servo variable for the sun
 
+//Pin number for the led
+const int sunledPin = 13;
+
 //Tower button set-up
 const int buttonTPin = 4;  //Placeholder pin number
 int buttonTState = 0;
@@ -20,13 +23,26 @@ int buttonSState = 0;
 int prevButtonSState = 0;
 bool S = false;
 
+//Servo counter setup
+
+
+//Tower button set-up
+const int buttonRPin = 2;  //Placeholder pin number
+int buttonRState = 0;
+int prevButtonRState = 0;
+bool R = false;
+
 void setup() {
   sunservo.attach(9);
   towerservo.attach(10);
+  pinMode(buttonRPin, INPUT);
   pinMode(buttonTPin, INPUT);
   pinMode(buttonSPin, INPUT);
+  pinMode(sunledPin, OUTPUT);
   Serial.begin(9600);
   Serial.println("Start Tower Button");
+  towerservo.write(0);
+  sunservo.write(0);
 }
 
 void loop() {
@@ -36,7 +52,7 @@ void loop() {
   if (buttonTState != prevButtonTState) {
     if (buttonTState == HIGH) {
       T = true;
-      towerservo.write(90);
+      towerservo.write(110);
       Serial.println("Start Sun button");
     } else {
       towerservo.write(0);
@@ -45,11 +61,12 @@ void loop() {
 
   //if tower button is true, read the sun button
   if (T == true) {
+    digitalWrite(sunledPin, HIGH);
     buttonSState = digitalRead(buttonSPin);
     if (buttonSState != prevButtonSState) {
       if (buttonSState == HIGH) {
         S = true;
-        sunservo.write(90);
+        sunservo.write(110);
         Serial.println("Start Sun button");
       } else {
         sunservo.write(0);
@@ -61,6 +78,20 @@ void loop() {
     Serial.println("The End");
   }
 
+  if (T == true && S == true) {
+   buttonRState = digitalRead(buttonRPin);
+    if (buttonRState != prevButtonRState) {
+      if (buttonRState == HIGH) {
+        R == true;
+        S == false;
+        digitalWrite(sunledPin, LOW);
+      }
+    }
+  }
+
   prevButtonTState = buttonTState;
   prevButtonSState = buttonSState;
+  prevButtonRState = buttonRState;
 }
+
+//Practice for the counter code
